c6ffe5f0b0c9d3e56f6fca7c401ca2302754e4d7
Glitch (web-dev-hello-webpage) <none>
2021-11-24 14:22:48 +0000

    🎏💠 Checkpoint ./app.js:34669905/1125 ./index.html:34669905/7

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 window.addEventListener("DOMContentLoaded", async event => {
+  checkRegistration();
+  
+  document.querySelector("#register").addEventListener("click", register);
+  document.querySelector("#unregister").addEventListener("click", unregister);
+  
+});
+
+// Check a service worker registration status
+async function checkRegistration() {
   if ('serviceWorker' in navigator) {
       const registration = await navigator.serviceWorker.getRegistration();
       if (registration) {
@@ -9,13 +18,10 @@ window.addEventListener("DOMContentLoaded", async event => {
   } else {
       showResult("Service workers API not available");
   }
-  
-  document.querySelector("#register").addEventListener("click", register);
-  document.querySelector("#unregister").addEventListener("click", unregister);
-  
-});
+}
 
-async function register(event) {
+// Registers a service worker
+async function register() {
   if ('serviceWorker' in navigator) {
     try {
       // Change the service worker URL to see what happens when the SW doesn't exist
@@ -23,26 +29,27 @@ async function register(event) {
       showResult("Service worker registered");
        
     } catch (error) {
-      showResult("Service worker not registered. " + error.message);
+      showResult("Error while registering: " + error.message);
     }    
   } else {
       showResult("Service workers API not available");
   }
 }; 
 
-async function unregister(event)  {
+// Unregister a currently registered service worker
+async function unregister()  {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.getRegistration();
       if (registration) {
-        const result = registration.unregister();
+        const result = await registration.unregister();
         showResult(result ? "Service worker unregistered" : "Service worker couldn't be unregistered");
       } else {
         showResult("There is no service worker to unregister");
       }
        
     } catch (error) {
-      showResult("Service worker not unregistered. " + error.message);
+      showResult("Error while unregistering: " + error.message);
     }    
   } else {
       showResult("Service workers API not available");
